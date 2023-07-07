339f389a8a0aa3add2cf7e27d3fac60e671344bf
Alejandro Rozas <alejandro.rozas@gmail.com>
2023-07-07 12:29:10 -0300

    Add Catalog API first commit.

---
+++ Catalog/CatalogApi/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "This API is running..";
+        }
+    }
+}
---
+++ Catalog/CatalogDomain/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace CatalogDomain
+{
+    public class Product
+    {
+
+    }
+}
