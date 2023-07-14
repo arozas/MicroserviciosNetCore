c6dced1ceae7a0083666c49c12fdf59fac6247f9
Alejandro Rozas <alejandro.rozas@gmail.com>
2023-07-13 23:46:45 -0300

    Added Startup.cs as in dotNet 5, to uncouple app configuration from Program.cs

---
+++ Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Catalog.API;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        
+    }
+
+    public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            applicationBuilder.UseDeveloperExceptionPage();
+        }
+
+        applicationBuilder.UseRouting();
+        applicationBuilder.UseStaticFiles();
+        applicationBuilder.UseAuthorization();
+        applicationBuilder.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions());
+        });
+    }
+    
+}
