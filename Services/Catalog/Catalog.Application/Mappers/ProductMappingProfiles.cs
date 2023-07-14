453633eb45f4fca228dfd5440b77974522030e0d
Alejandro Rozas <alejandro.rozas@gmail.com>
2023-07-14 13:56:06 -0300

    Added MediaTR and AutoMapper dependencies, GetAllBrands CQRS and Mediator Pattern

---
+++ Services/Catalog/Catalog.Application/Mappers/ProductMappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Mappers;
+
+public class ProductMappingProfiles:Profile
+{
+    public ProductMappingProfiles()
+    {
+        CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+    }
+}
