1d1ad1be07140ae43b61c5f2992098aaa5fa8254
Alejandro Rozas <alejandro.rozas@gmail.com>
2023-07-14 09:30:41 -0300

    Added context implementation and mongo database settings

---
+++ Catalog/Catalog.Infraestructure/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infraestructure.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; set; }
+    public IMongoCollection<ProductBrand> Brands { get; set; }
+    public IMongoCollection<ProductType> Types { get; set; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+        Brands = database.GetCollection<ProductBrand>(configuration.GetValue<string>("DatabaseSetting:BrandsCollections"));
+        Types = database.GetCollection<ProductType>(configuration.GetValue<string>("DatabaseSetting:TypesCollections"));
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetting:ProductsCollections"));
+        
+        BrandContextSeed.SeedData(Brands);
+        TypeContextSeed.SeedData(Types);
+        CatalogContextSeed.SeedData(Products);
+    }
+}
---
+++ Catalog/Catalog.Infraestructure/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infraestructure.Data;
+
+public interface ICatalogContext
+{
+    IMongoCollection<Product> Products { get; set; }
+    IMongoCollection<ProductBrand> Brands { get; set; }
+    IMongoCollection<ProductType> Types { get; set; }
+}
