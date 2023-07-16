4bb7c133e6c90c1635075599db00efb400e1d201
Alejandro Rozas <alejandro.rozas@gmail.com>
2023-07-16 13:36:22 -0300

    Added methods to controller and Docker file.

--- Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,6 +1,105 @@
-﻿namespace Catalog.API.Controllers;
+﻿using System.Net;
+using System.Runtime.CompilerServices;
+using Catalog.Application.Commands;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
 
 public class CatalogController : ApiController
 {
+    private readonly IMediator _mediator;
+
+    public CatalogController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}", Name = "GetProductById")]
+    [ProducesResponseType(typeof(ProductResponse),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ProductResponse>> GetProductById(string id)
+    {
+        var query = new GetProductByIdQuery(id);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
     
-}
+    [HttpGet]
+    [Route("[action]/{productName}", Name = "GetProductByName")]
+    [ProducesResponseType(typeof(IList<ProductResponse>),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IList<ProductResponse>>> GetProductByName(string productName)
+    {
+        var query = new GetProductByNameQuery(productName);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [Route("GetAllProducts")]
+    [ProducesResponseType(typeof(IList<ProductResponse>),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IList<ProductResponse>>> GetAllProduct()
+    {
+        var query = new GetAllProductsQuery();
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [Route("GetAllBrands")]
+    [ProducesResponseType(typeof(IList<BrandResponse>),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IList<BrandResponse>>> GetAllBrands()
+    {
+        var query = new GetAllBrandsQuery();
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [Route("GetAllTypes")]
+    [ProducesResponseType(typeof(IList<TypeResponse>),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IList<TypeResponse>>> GetAllTypes()
+    {
+        var query = new GetAllTypesQuery();
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    [Route("[action]/{brandName}", Name = "GetProductsByBrandName")]
+    [ProducesResponseType(typeof(IList<ProductResponse>),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IList<ProductResponse>>> GetProductsByBrandName(string brandName)
+    {
+        var query = new GetProductByBrandQuery(brandName);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+    [HttpPost]
+    [Route("CreateProduct")]
+    [ProducesResponseType(typeof(ProductResponse),(int)HttpStatusCode.Created)]
+    public async Task<ActionResult<ProductResponse>> CreateProduct([FromBody] CreateProductCommand productCommand)
+    {
+        var result = await _mediator.Send(productCommand);
+        return Created($@"GetProductById/{result.Id}",result);
+    }
+    
+    [HttpPut]
+    [Route("UpdateProduct")]
+    [ProducesResponseType(typeof(ProductResponse),(int)HttpStatusCode.OK)]
+    public async Task<IActionResult> UpdateProduct([FromBody] UpdateProductCommand productCommand)
+    {
+        var result = await _mediator.Send(productCommand);
+        return Ok(result);
+    }
+    [HttpDelete]
+    [Route("{id}",Name="DeleteProduct")]
+    [ProducesResponseType(typeof(ProductResponse),(int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteProduct(string id)
+    {
+        var query = new DeleteProductByIdQuery(id);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}   
